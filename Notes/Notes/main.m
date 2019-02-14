35122c1dca850cbae32ca6cf7c75ca64838f7d2a
zyl <zyl@bogon.dhcp>
2019-02-14 14:28:56 +0800

    Privacy权限

---
+++ Notes/Notes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Notes
+//
+//  Created by zyl on 2019/2/14.
+//  Copyright © 2019 zyl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
